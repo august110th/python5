e4556188e00955e800ca7f0ce748290921a3917e
august110th <august110th@ya.ru>
2021-06-14 21:44:07 +0400

    done

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 from pprint import pprint
 # читаем адресную книгу в формате CSV в список contacts_list
 import csv
@@ -5,7 +6,11 @@ with open("phonebook_raw.csv", encoding='utf-8') as f:
   rows = csv.reader(f, delimiter=",")
   contacts_list = list(rows)
 pprint(contacts_list)
-
+# (\+7|8)\s*\(*495\)*\s*\-*\d{3}\-*\d{2}\-*\d+(\s*\(*доб.\s*\d+\)*)*
+# (\+7|8)\s*\(*(495)\)*\s*\-*(\d{3})\-*(\d{2})\-*(\d+)(\s*\(*(доб.)\s*(\d+)\)*)*
+pattern = r'(\+7|8)\s*\(*(495)\)*\s*\-*(\d{3})\-*(\d{2})\-*(\d+)(\s*\(*(доб.)\s*(\d+)\)*)*'
+substitution = r'+7(\2)\3-\4-\5 \7\8'
+result = re.sub(pattern, substitution,contacts_list)
 
 # with open("phonebook.csv", "w", newline='') as f:
 #   datawriter = csv.writer(f, delimiter=',')
