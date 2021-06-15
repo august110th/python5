f72f30f9c0f46b7e7a1034d734057ce49f3d1a1d
august110th <80058477+august110th@users.noreply.github.com>
2021-06-15 11:18:57 +0400

    done

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ import csv
 with open("phonebook_raw.csv", encoding='utf-8') as f:
   rows = csv.reader(f, delimiter=",")
   contacts_list = list(rows)
-pprint(contacts_list)
-# (\+7|8)\s*\(*495\)*\s*\-*\d{3}\-*\d{2}\-*\d+(\s*\(*доб.\s*\d+\)*)*
-# (\+7|8)\s*\(*(495)\)*\s*\-*(\d{3})\-*(\d{2})\-*(\d+)(\s*\(*(доб.)\s*(\d+)\)*)*
+# pprint(contacts_list)
+
 pattern = r'(\+7|8)\s*\(*(495)\)*\s*\-*(\d{3})\-*(\d{2})\-*(\d+)(\s*\(*(доб.)\s*(\d+)\)*)*'
 substitution = r'+7(\2)\3-\4-\5 \7\8'
-result = re.sub(pattern, substitution,i) for i in contacts_list
+for i in contacts_list:
+  for v in i:
+    result = re.sub(pattern, substitution, v)
+    print(result)
+# result = [re.sub(pattern, substitution,i) for i in contacts_list]
+# print(result)
 
 # with open("phonebook.csv", "w", newline='') as f:
 #   datawriter = csv.writer(f, delimiter=',')
