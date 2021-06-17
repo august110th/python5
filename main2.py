fc8ad6165c4f25b3aa7a4871c4537d3e0ab21f29
august110th <august110th@ya.ru>
2021-06-17 15:19:55 +0400

    done

--- main2.py
+++ main2.py
@@ -22,27 +22,4 @@ substitution = r'+7(\2)\3-\4-\5 \7\8'
 #   datawriter.writerows(contacts_list)
 
 
-class Corrector:
-    def __init__(self):
-      self.contacts_list = []
 
-    def csv_reader(self, file):
-      with open(file, encoding='utf-8') as f:
-        rows = csv.reader(f, delimiter=",")
-        self.contacts_list = list(rows)
-
-    def csv_writer(self, file):
-      with open(file, "w", newline='') as f:
-        datawriter = csv.writer(f, delimiter=',')
-        # Вместо contacts_list подставьте свой список
-        datawriter.writerows(self.contacts_list)
-
-    def __iter__(self):
-      return self
-
-    def __next__(self):
-      for item in self.contacts_list:
-        print(item)
-        self.result = re.sub(r'(\+7|8)\s*\(*(495)\)*\s*\-*(\d{3})\-*(\d{2})\-*(\d+)(\s*\(*(доб.)\s*(\d+)\)*)*', r'+7(\2)\3-\4-\5 \7\8', item)
-
-k = Corrector()
